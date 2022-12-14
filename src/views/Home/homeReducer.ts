fc340092a53c2bd288e8fe392598719bdc69b1c2
MaxLeduc <leduc.maximep@gmail.com>
2022-12-14 17:56:40 -0500

    removed commented out code

--- src/views/Home/homeReducer.ts
+++ src/views/Home/homeReducer.ts
@@ -10,7 +10,6 @@ type StateTypes =
 export type AppState = {
   card: Card | null
   currentState: StateTypes
-  // card: Card
 }
 
 type ShowCardAction = {
@@ -43,23 +42,9 @@ export type Actions =
   | StopReading
   | Restart
 
-const TempCard: Card = {
-  id: 0,
-  slug: 'the_fool',
-  description:
-    'On The Fool Tarot card, a young man stands on the edge of a cliff, without a care in the world, as he sets out on a new adventure. He is gazing upwards toward the sky (and the Universe) and is seemingly unaware that he is about to skip off a precipice into the unknown. Over his shoulder rests a modest knapsack containing everything he needs – which isn’t much (let’s say he’s a minimalist). The white rose in his left hand represents his purity and innocence. And at his feet is a small white dog, representing loyalty and protection, that encourages him to charge forward and learn the lessons he came to learn. The mountains behind The Fool symbolize the challenges yet to come. They are forever present, but The Fool doesn’t care about them right now; he’s more focused on starting his expedition.',
-  upright: 'new beginnings, innocence, adventure',
-  type: 'Major Arcana',
-  wiki_link: 'https://en.wikipedia.org/wiki/The_Fool_(tarot_card)',
-  url: '/assets/the_fool.svg',
-  secret_type: 'Great Secret',
-}
-
 export const initialState: AppState = {
   card: null,
   currentState: 'INITIAL',
-  // card: TempCard,
-  // currentState: 'CARD_SHOWING',
 }
 
 export const reducer = (state: AppState, action: Actions) => {
